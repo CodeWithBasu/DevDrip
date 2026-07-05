dc426d45670f119651ae220652a38ce18095fe4e
Basudev Moharana <basudevmuna111@gmail.com>
2026-07-05 20:14:31 +0530

    chore: properly ignore .next build artifacts

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"LCmo/+LR6dAvKmZyqKozypdyml0qqeFf1Lj5Uob3IPw=\"\n}"
--- .next/dev/static/chunks/src_app_page_jsx_1r2tbvd._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_micromark-core-commonmark_dev_lib_2079wuz._.js","static/chunks/node_modules_parse5_dist_1nrczq8._.js","static/chunks/node_modules_1avgjt6._.js","static/chunks/src_1tm5dai._.js"],
-    source: "dynamic"
-});
